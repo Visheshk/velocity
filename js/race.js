b2c5654a8cb87d61fb7492a7318a0e48beb62454
Vishesh Kumar <vishesh@visheshk.net>
2013-08-11 02:02:52 +0530

    Camera of car 1, just the trees, working

--- js/race.js
+++ js/race.js
@@ -2,7 +2,7 @@ var c1speed = 0, c2speed = 0, update = 0, c1speedDisp = 0 ,c2speedDisp = 0, paus
 var trees = new Layer();
 var cars = new Layer();
 var buttons = new Layer();
-var camera1 = new Layer();
+
 
       var hitOptions = {
         segments: false,
@@ -40,25 +40,25 @@ var camera1 = new Layer();
       trees.activate();
       var tree = new Raster('tree');
       tree.position = new Point(10, 10);
-      for (var i = 0; i < 20; i++) {
+      for (var i = 0; i < 7; i++) {
         var copy = tree.clone();
         copy.position.x += 2 * i * copy.bounds.width;
       }
 
       var tree2 = tree.clone();
       tree2.position = new Point(10, 400);
-      for (var i = 0; i < 20; i++) {
+      for (var i = 0; i < 7; i++) {
         var copy = tree2.clone();
         copy.position.x += 2 * i * copy.bounds.width;
       }
 
       buttons.activate();
-      var p = new Rectangle(new Point(600, 40), new Point(650, 80));
+      var p = new Rectangle(new Point(500, 40), new Point(550, 80));
       var pauseB = new Path.RoundRectangle(p, 2);
       pauseB.name = 'pause';
       pauseB.fillColor = '#ffffff';
       pauseB.strokeColor = '#222222';
-      pauseBT = new PointText(new Point(610, 65)); // pause Button Text
+      pauseBT = new PointText(new Point(510, 65)); // pause Button Text
       pauseBT.name = 'pauseText';
       pauseBT.fillColor = 'black';
       pauseBT.content = 'Pause';
@@ -68,24 +68,24 @@ var camera1 = new Layer();
       /*var c1 = new Rectangle(new Point(100, 100), new Point(200, 150));
       car1 = new Path.RoundRectangle(c1, 5);*/
       car1 = new Raster('car1');
-      car1.position = new Point(125, 125);
+      car1.position = new Point(125, 175);
       car1.name = 'car1';
       car1.fillColor = '#cccccc';
       car1.strokeColor = '#222222';
-      car1s = new PointText(new Point(120, 130));
+      car1s = new PointText(new Point(120, 180));
       car1s.name = 'car1s';
       car1s.fillColor = 'white';
       car1s.content = c1speed;
 
       var car2, car2s;
-      var c2 = new Rectangle(new Point(100, 175), new Point(200, 225));
+      var c2 = new Rectangle(new Point(100, 225), new Point(200, 275));
       //car2 = new Path.RoundRectangle(c2, 5);*/
       car2 = new Raster('car2');
-      car2.position = new Point(125, 200);
+      car2.position = new Point(125, 300);
       car2.name = 'car2';
       car2.fillColor = '#ddeedd';
       car2.strokeColor = '#222222';
-      car2s = new PointText(new Point(120, 205));
+      car2s = new PointText(new Point(120, 305));
       car2s.name = 'car2s';
       car2s.fillColor = 'white';
       car2s.content = c1speed;
@@ -98,24 +98,53 @@ var camera1 = new Layer();
       butt2.name = 'car2';
       butt2.fillColor = '#ddeedd';
       butt2.strokeColor = '#222222';
-      butt2s = new PointText(new Point(90, 205));
+      butt2s = new PointText(new Point(90, 255));
       butt2s.name = 'car2s';
       butt2s.fillColor = 'black';
       butt2s.content = 'Toggle car 2';
 
 
             
-      camera1.clipped = true
+      
       
       //camera1.bounds = rect;
-      camera1.activate();
-      var rect = new Path.Rectangle(new Point(600, 40), new Point(800, 140));
-      rect.strokeColor = 'grey';
-      rect.fillColor = 'black';
-      rect.strokeWidth = 5;
-      var c3 = car2.clone();
-      c3.position = camera1.position;
+      
+      var rect = new Path.Rectangle(new Point(590, 0), new Point(810, 130));
+      
+      
+      var camera1 = new Layer(rect);
+      camera1.clipped = true;
+      camera1.opacity = 1;
+      var rect2 = new Path.Rectangle(new Point(595, 5), new Point(805, 125));
+      //rect2.scale(0.8);
+      rect2.fillColor = 'white';
+      //rect2.opacity = 1;
+      rect2.strokeWidth = 15;
+      rect2.strokeColor = 'grey';
+      
+      var trees3 = new Group();
+      trees3.opacity = 0.8;
+      var tree3 = tree2.clone();
+      tree3.position = new Point(10, 60);
+      trees3.addChild(tree3);
 
+      for (var i = 0; i < 20; i++) {
+        var copy = tree3.clone();
+        copy.position.x += 2 * i * copy.bounds.width;
+        trees3.addChild(copy);
+      }
+      trees3.bounds.x = rect.bounds.x - car1.bounds.x;
+      
+      trees3.scale(-1, -1);
+      trees3.rotate(180, rect.bounds.center);
+      console.log(trees3.bounds.x + trees3.bounds.width - rect.bounds.x - rect.bounds.width, rect.bounds.x, car1.bounds.x + car1.bounds.width);
+      //trees3.bounds.x = rect.bounds.x - car1.bounds.x;
+      //console.log(trees3.bounds.x, rect.bounds.x, car1.bounds.x);
+      //trees3.bounds.x = rect.bounds.x - car1.bounds.x;
+      //console.log(trees3.bounds.x, rect.bounds.x, car1.bounds.x);
+      //var c3 = car2.clone();
+      //c3.position = camera1.bounds.center;
+      
 
 
 
@@ -152,7 +181,7 @@ var camera1 = new Layer();
         var obj;
         //butt2.fillColor = '#ddeedd';
         //pauseB.fillColor = '#ffffff';
-        project.layers[0].selected = false;
+        buttons.selected = false;
         if (hitResult){
           obj = hitResult.item;
         }
@@ -177,6 +206,17 @@ var camera1 = new Layer();
         }
       }
 
+      function updateCamera(){
+        //trees3.bounds.x = camera1.bounds.x - car1.bounds.x;
+        //console.log(trees3.bounds.x, camera1.bounds.x, car1.bounds.x);
+        //trees3.visible = false;
+        //trees3.bounds.x = rect.bounds.x - car1.bounds.x;
+        //trees3.scale(-1, -1);
+        //trees3.rotate(180, rect.bounds.center);
+        //trees3.visible = true;
+        trees3.bounds.x = rect.bounds.x + rect.bounds.width - trees3.bounds.width + car1.bounds.x;
+      }
+
       function updateSpeed(){
         car1.right = car1.bounds.x + car1.bounds.width;
         car1.left = car1.bounds.x;
@@ -195,6 +235,9 @@ var camera1 = new Layer();
             car1s.content = c1speed;
             car2s.content = c2speed;
           }
+        }
+        else if (car2Motion == 0){
+          car1s.content = c1speed;
         }
       }
 
@@ -237,6 +280,7 @@ var camera1 = new Layer();
           moveCar(1);
           moveCar(2);
         }
+        updateCamera();
         
       }
 
